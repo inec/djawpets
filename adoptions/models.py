44753297f1a70e47e9e7adcd29f4c9eecea52085
inec <luyege+github@gmail.com>
2018-12-26 00:39:47 -0600

    model basic

--- adoptions/models.py
+++ adoptions/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 class Pet(models.Model):
+        name=models.CharField(max_length=100)
+        submitter=models.CharField(max_length=100)
+        species=models.CharField(max_length=30)
+        breed=models.CharField(max_length=30,blank=True)
+        
