eb9c51bb8732bf8bee8af1f944708ae151e976cf
inec <luyege+github@gmail.com>
2018-12-26 00:43:52 -0600

    'GENDER_CHOICES'

--- adoptions/models.py
+++ adoptions/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 
 class Pet(models.Model):
+        SEX_CHOICES=[('M','Male'),('F','Female')]
         name=models.CharField(max_length=100)
         submitter=models.CharField(max_length=100)
         species=models.CharField(max_length=30)
         breed=models.CharField(max_length=30,blank=True)
+        description = models.TextField()
+        sext=models.CharField(choices=SEX_CHOICES)
+        
         
