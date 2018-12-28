68968d15b1eb6a07da79b12c14b05f31680d8d8d
inec <luyege+github@gmail.com>
2018-12-27 22:59:32 -0600

    'test'

--- adoptions/views.py
+++ adoptions/views.py
@@ -5,5 +5,5 @@ def home(request):
     return HttpResponse('<p>home view </p>')
 
 
-def pet_details(request,id):    
+def pet_detail(request,id):    
     return HttpResponse('<p>deatl {}</p>'.format(id))
