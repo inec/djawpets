e50454f392248e9fd5981e91e4149fb690bcfb85
inec <luyege+github@gmail.com>
2018-12-27 23:15:00 -0600

    'test'

--- adoptions/views.py
+++ adoptions/views.py
@@ -5,15 +5,15 @@ from django.http import Http404
 from .models import Pet
 
 def home(request):
-    pet=Pet.objects.all()
+    pets=Pet.objects.all()
     return render(request,'home.html',{'pets':pets})
     #return HttpResponse('<p>home view </p>')
 
 
 def pet_detail(request,id):  
     try:  
-        pet=Pet.object.get(id=id)
+        pet=Pet.objects.get(id=id)
     except Pet.DoesNotExist:
         raise Http404('pet not found')
-    return render(request,'pet_home.html',{'pet':pet})  
+    return render(request,'pet_detail.html',{'pet':pet})  
     #return HttpResponse('<p>deatl {}</p>'.format(id))
