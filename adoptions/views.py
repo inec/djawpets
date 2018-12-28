541130b31aab4b8b1a953be988308a39adf9db26
inec <luyege+github@gmail.com>
2018-12-27 23:10:28 -0600

    'etst'

--- adoptions/views.py
+++ adoptions/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import Http404
+
+from .models import Pet
 
 def home(request):
-    return HttpResponse('<p>home view </p>')
+    pet=Pet.objects.all()
+    return render(request,'home.html',{'pets':pets})
+    #return HttpResponse('<p>home view </p>')
 
 
-def pet_detail(request,id):    
-    return HttpResponse('<p>deatl {}</p>'.format(id))
+def pet_detail(request,id):  
+    try:  
+        pet=Pet.object.get(id=id)
+    except Pet.DoesNotExist:
+        raise Http404('pet not found')
+    return render(request,'pet_home.html',{'pet':pet})  
+    #return HttpResponse('<p>deatl {}</p>'.format(id))
